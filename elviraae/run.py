c95236f016e4d2c6fccc322c2d25f9e18c67bce9
Miguel Miño Alava <jimmy.mino@itsae.edu.ec>
2015-01-22 16:58:46 -0500

    Update run.py

--- elviraae/run.py
+++ elviraae/run.py
@@ -7,3 +7,5 @@ from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    #alguien andubo x aqui  iniciales JMMA   jajajajajajajajajaja waaaaaaaaaaaaa 1 2 3 4 5 6 7 8 9 10
